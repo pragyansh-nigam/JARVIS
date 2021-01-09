d55ab0af30c53f209ce8c45246f5f4e45e38e6ea
Pragyansh Nigam <nigampragyansh@gmail.com>
2021-01-09 19:47:40 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -47,7 +47,7 @@ def recordAudio():
 def start():
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
-        speak("Good Morning Pragyansh")
+        speak("Good Morning Nigam")
     
     elif hour>=12 and hour<=15:
         speak("Good Afternoon Pragyansh")
